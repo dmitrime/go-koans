913c97c360f848c7870574845e0c76f6d63fb04b
dmitrime <dmitrime@github.com>
2015-09-26 16:02:55 +0200

    common interfaces done

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -1,32 +1,35 @@
 package go_koans
 
 import "bytes"
+import "io"
 
 func aboutCommonInterfaces() {
-  {
-    in := new(bytes.Buffer)
-    in.WriteString("hello world")
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
 
-    out := new(bytes.Buffer)
+		out := new(bytes.Buffer)
+		io.Copy(out, in)
 
-    /*
-       Your code goes here.
-       Hint, use these resources:
+		/*
+		   Your code goes here.
+		   Hint, use these resources:
 
-       $ godoc -http=:8080
-       $ open http://localhost:8080/pkg/io/
-       $ open http://localhost:8080/pkg/bytes/
-    */
+		   $ godoc -http=:8080
+		   $ open http://localhost:8080/pkg/io/
+		   $ open http://localhost:8080/pkg/bytes/
+		*/
 
-    assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
-  }
+		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
+	}
 
-  {
-    in := new(bytes.Buffer)
-    in.WriteString("hello world")
+	{
+		in := new(bytes.Buffer)
+		in.WriteString("hello world")
 
-    out := new(bytes.Buffer)
+		out := new(bytes.Buffer)
+		io.CopyN(out, in, 5)
 
-    assert(out.String() == "hello") // duplicate only a portion of the io.Reader
-  }
+		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
+	}
 }
